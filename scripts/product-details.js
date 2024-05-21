29d6d214d3796e748c5731903cf3ecc04ca41a4e
Izzeldin <90211318+izzeldin-b@users.noreply.github.com>
2024-05-21 22:50:34 +0700

    Replace With React Implementation

--- scripts/product-details.js
+++
@@ -1,9 +0,0 @@
-const buttons = document.querySelectorAll('.product-variants-choice-button');
-
-buttons.forEach(button => {
-    button.addEventListener('click', () => {
-        buttons.forEach(btn => btn.classList.remove('product-variants-choice-button-selected'));
-
-        button.classList.add('product-variants-choice-button-selected');
-    });
-});
