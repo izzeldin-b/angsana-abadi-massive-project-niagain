b3b813079a3a27f05355ff29e3900fa9def59e21
Izzeldin <izzelbas12@gmail.com>
2024-06-21 13:55:06 +0700

    Updated Promo Carousel Image Pathing For Hosting
    Changed the pathing of images in the promo carousel so that Vercel will display the image during hosting.

--- client/src/components/PromoCarousel.jsx
+++ client/src/components/PromoCarousel.jsx
@@ -1,14 +1,17 @@
 import React from 'react'
+import Banner1 from '/images/new-banner-1.jpg'
+import Banner2 from '/images/new-banner-2.jpg'
+import Banner3 from '/images/new-banner-3.png'
 
 function PromoCarousel() {
     return (
         <div>
             <div className="image-slideshow-container">
                 <div className="image-slideshow-container-wrapper">
-                    <img src="src\assets\images\banner-slideshow\new-banner-1.jpg"/>
-                    <img src="src\assets\images\banner-slideshow\new-banner-2.jpg"/>
-                    <img src="src\assets\images\banner-slideshow\new-banner-3.png"/>
-                    <img src="src\assets\images\banner-slideshow\new-banner-1.jpg"/>
+                    <img src={Banner1}/>
+                    <img src={Banner2}/>
+                    <img src={Banner3}/>
+                    <img src={Banner1}/>
                 </div>
             </div>
         </div>
