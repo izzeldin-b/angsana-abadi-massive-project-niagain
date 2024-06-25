8a33301677efd10ae57aa48da19401e017f94d40
Izzeldin <izzelbas12@gmail.com>
2024-06-25 11:54:47 +0700

    Added Crucial Notification To Handle Non-Running API
    Added hideable notification on the main page to instruct the user on what to do when basic functionality such as viewing products, etc does not work.

--- client/src/pages/Main.jsx
+++ client/src/pages/Main.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom'
 import ScrollToTop from '../components/ScrollToTop'
 import TopServiceMain from '../components/TopServiceMain'
@@ -8,11 +8,38 @@ import '../assets/styles/main-page.css'
 
 function Main() {
 
+    const [showBox, setShowBox] = useState(true);
+
+    const toggleBox = () => {
+        setShowBox(!showBox);
+    };
+
     return (
         <div>
             <ScrollToTop />
                 <div className="main-page">
                     <div className="main-page-container">
+
+                        <div
+                            className={`sticky-box ${showBox ? 'box' : 'button'}`}
+                            onClick={toggleBox}
+                        >
+                            {showBox ? (
+                                <div>
+                                    <span>Perhatian</span> <br/><br/>Jika daftar produk tidak terlihat atau fungsionalitas
+                                    tidak berjalan, silahkan refresh halaman dan <b>tunggu sekitar 1 menit </b> 
+                                    dengan tujuan menyalakan kembali API. <br/><br/>Hal ini terjadi karena API
+                                    otomatis dimatikan oleh Render jika tidak ada permintaan dalam waktu
+                                    dekat. <br/><br/>Catatan ini akan dihapus ketika kami menggunakan service
+                                    atau plan API yang selalu berjalan tanpa adanya downtime. <br/><br/>
+                                    <div id='smaller-notification-text'>Klik untuk Tutup</div>
+                                </div>
+                            ) : (
+                                <div id='smaller-notification-text'>
+                                    Tampilkan Pengumuman
+                                </div>
+                            )}
+                        </div>
 
                         {/* CAROUSEL */}
                         <PromoCarousel/>
